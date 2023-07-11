6aeb3eac6442566ad4d1298c072a0851278ffe2f
idingg <48832274+idingg@users.noreply.github.com>
2023-07-11 16:47:40 +0900

    Add files via upload
    초기 모듈 설치여부 확인 속도 개선
    모듈 설치 시 카카오 서버로 접속하도록 변경
    파이어폭스 브라우저 지원 추가
    브라우저 미확인 시 전용 크롬 모듈 설치 및 사용 추가
    브라우저 사용 우선순위 변경(크롬-엣지-파이어폭스-전용 크롬)
    해상도 입력 오류 처리 추가

--- weverse-live-download.py
+++ weverse-live-download.py
@@ -1,51 +1,64 @@
-import subprocess
-import sys
-import os
+import datetime
 from time import sleep
-
-print("필요파일 설치확인 중")
+import os
+import sys
+import subprocess
+print("\n* 저작권자의 동의 없이 콘텐츠를 무단 배포 및 가공하는 행위는 저작권 침해에 해당합니다.")
+print("* Unauthorized or illegal distribution of paid contents, which are available exclusively to those who paid for the subscription, is punishable by law.\n")
 
 
 def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "-q", "install", package])
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", package, "-i", "http://ftp.daumkakao.com/pypi/simple", "--trusted-host", "ftp.daumkakao.com"])
 
 
-install("requests")
-install("playwright")
-
-from playwright.sync_api import sync_playwright
-import requests
-import datetime
+print("필수모듈 설치확인 중")
+try:
+    from playwright.sync_api import sync_playwright
+except:
+    print("라이브영상 데이터 수집모듈이 발견되지 않아 설치합니다.")
+    install("playwright")
+    from playwright.sync_api import sync_playwright
+try:
+    import requests
+except:
+    print("라이브영상 데이터 다운로더가 발견되지 않아 설치합니다.")
+    install("requests")
+    import requests
 
 
 def main():
-    addr = ""
-    while addr == "" or addr.startswith("https://weverse.io/") == False:
-        addr = input("다운받을 위버스 라이브 주소 (입력예 : https://weverse.io/... ) : ")
-
     playwright = sync_playwright().start()
-
-    print("브라우저 실행 중")
-    # browser = playwright.chromium.launch(headless=True)
-
     try:
-        browser = playwright.chromium.launch(headless=True, channel="msedge")
+        browser = playwright.chromium.launch(headless=True, channel="chrome")
     except:
         try:
-            browser = playwright.chromium.launch(headless=True, channel="chrome")
+            browser = playwright.chromium.launch(
+                headless=True, channel="msedge")
         except:
-            print("브라우저를 설치 해주세요.")
-            exit(1)
+            try:
+                browser = playwright.firefox.launch(headless=True)
+            except:
+                print("브라우저가 발견되지 않아 전용 크롬 모듈을 설치합니다.")
+                subprocess.check_call(
+                    [sys.executable, "-m", "playwright", "install", "chromium"])
+                browser = playwright.chromium.launch(headless=True)
+
     page = browser.new_context().new_page()
     reqs = []
-    page.on("request", lambda request: reqs.append([request.method, request.url]))
-    print("요청 수집 중")
+    page.on("request", lambda request: reqs.append(
+        [request.method, request.url]))
+    addr = ""
+    while addr == "" or addr.startswith("https://weverse.io/") == False:
+        addr = input("다운받을 위버스 라이브 주소 (입력예 : https://weverse.io/... [엔터]) : ")
+
+    print("라이브영상 데이터 수집 중")
     page.goto(url=addr, wait_until="networkidle")
     browser.close()
     playwright.stop()
 
+    print("라이브영상 데이터 분석 중")
     m3u8_allurl = ""
-    print(".m3u8 파일 찾는 중")
     for item in reqs:
         if item[1].find(".m3u8") != -1:
             m3u8_allurl = item[1]
@@ -61,46 +74,49 @@ def main():
             print(resstr.split("=")[1], end=" ")
     print()
 
-    if len(resolutions) > 1 and int(resolutions[0].split("x")[0]) < int(
-        resolutions[1].split("x")[0]
-    ):
+    if len(resolutions) > 1 and int(resolutions[0].split("x")[0]) < int(resolutions[1].split("x")[0]):
         resolutions.reverse()
 
-    resolution_to_get = input(
-        "해상도 선택 (기본값 : "
-        + resolutions[0]
-        + ", 입력예 : 1080 또는 720 또는 1920x1080 또는 1280x720 ... ) : "
-    )
-    if resolution_to_get == "":
-        resolution_to_get = resolutions[0]
+    m3u8_1080filename = ""
+    while m3u8_1080filename == "":
+        resolution_to_get = input(
+            "해상도 선택 (기본값 : "
+            + resolutions[0]
+            + ", 입력예 : 1080[엔터], 1920[엔터], 1920x1080[엔터], ... ) : "
+        )
+        if resolution_to_get == "":
+            resolution_to_get = resolutions[0].split("x")[-1]
 
-    for item in m3u8_resolutions:
-        if item.find(resolution_to_get) != -1:
-            m3u8_1080filename = item.splitlines()[1]
-            break
+        for item in m3u8_resolutions:
+            res1 = str(item.splitlines()[0].split("x")[0].split("=")[-1])
+            res2 = str(item.splitlines()[0].split("x")[-1])
+            if res1 == resolution_to_get or res2 == resolution_to_get:
+                m3u8_1080filename = item.splitlines()[1]
+                break
     end = m3u8_allurl.find(".m3u8") + 5
     start = m3u8_allurl[:end].rfind("/")
-    m3u8_1080url = m3u8_allurl.replace(m3u8_allurl[start + 1 : end], m3u8_1080filename)
+    m3u8_1080url = m3u8_allurl.replace(
+        m3u8_allurl[start + 1: end], m3u8_1080filename)
 
     first_content = b""
     new_content = requests.get(m3u8_1080url).content
 
     # save to file
-
     if m3u8_1080url.find("live") != -1:
         path = new_content[
             new_content[: new_content.find(b".ts") + 3].rfind(b"\n")
-            + 1 : new_content.find(b".ts")
+            + 1: new_content.find(b".ts")
             + 3
         ].decode()
     elif m3u8_1080url.find("VOD") != -1:
-        path = m3u8_1080filename[m3u8_1080filename.rfind("/") + 1 :].replace(
+        path = m3u8_1080filename[m3u8_1080filename.rfind("/") + 1:].replace(
             ".m3u8", ".ts", 1
         )
 
     one_ts = open("./" + path, "wb+")
     while new_content != first_content:
-        tsfiles = new_content.replace(first_content, b"", 1).decode().split(sep=",")
+        tsfiles = new_content.replace(
+            first_content, b"", 1).decode().split(sep=",")
         if m3u8_1080url.find("live") != -1:
             end = len(m3u8_1080url)
         elif m3u8_1080url.find("VOD") != -1:
@@ -111,7 +127,8 @@ def main():
         for item in tsfiles:
             if item.find(".ts") != -1:
                 tmp = item.splitlines()[1]
-                tsurls.append(m3u8_1080url.replace(m3u8_1080url[start + 1 : end], tmp))
+                tsurls.append(m3u8_1080url.replace(
+                    m3u8_1080url[start + 1: end], tmp))
 
         cnt = 1
         downloadsize = 0
@@ -149,7 +166,7 @@ def main():
             cnt += 1
 
         if m3u8_1080url.find("live") != -1:
-            print("\n30초동안 이어지는 영상을 확인합니다.")
+            print("\n30초간 이어지는 영상이 있는지 확인 후 저장합니다. 라이브 종료까지 반복됩니다.")
             one_ts.close()
             sleep(30)
             one_ts = open("./" + path, "ab+")
