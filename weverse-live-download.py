4d80cd8e59dbda795b198c07b31788e72f128b55
idingg <48832274+idingg@users.noreply.github.com>
2023-07-12 02:15:03 +0900

    Add files via upload
    멀티코어 지원

--- weverse-live-download.py
+++ weverse-live-download.py
@@ -3,28 +3,17 @@ from time import sleep
 import os
 import sys
 import subprocess
-print("\n* 저작권자의 동의 없이 콘텐츠를 무단 배포 및 가공하는 행위는 저작권 침해에 해당합니다.")
-print("* Unauthorized or illegal distribution of paid contents, which are available exclusively to those who paid for the subscription, is punishable by law.\n")
+from multiprocessing import Pool
 
 
 def install(package):
     subprocess.check_call(
-        [sys.executable, "-m", "pip", "install", package, "-i", "http://ftp.daumkakao.com/pypi/simple", "--trusted-host", "ftp.daumkakao.com"])
+        [sys.executable, "-m", "pip", "-q", "install", package, "-i", "http://ftp.daumkakao.com/pypi/simple", "--trusted-host", "ftp.daumkakao.com"])
 
 
-print("필수모듈 설치확인 중")
-try:
-    from playwright.sync_api import sync_playwright
-except:
-    print("라이브영상 데이터 수집모듈이 발견되지 않아 설치합니다.")
-    install("playwright")
-    from playwright.sync_api import sync_playwright
-try:
-    import requests
-except:
-    print("라이브영상 데이터 다운로더가 발견되지 않아 설치합니다.")
-    install("requests")
-    import requests
+def downloadts(url: str):
+    from requests import get
+    return get(url)
 
 
 def main():
@@ -52,12 +41,12 @@ def main():
     while addr == "" or addr.startswith("https://weverse.io/") == False:
         addr = input("다운받을 위버스 라이브 주소 (입력예 : https://weverse.io/... [엔터]) : ")
 
-    print("라이브영상 데이터 수집 중")
+    print("라이브 데이터 수집 중")
     page.goto(url=addr, wait_until="networkidle")
     browser.close()
     playwright.stop()
 
-    print("라이브영상 데이터 분석 중")
+    print("라이브 데이터 분석 중")
     m3u8_allurl = ""
     for item in reqs:
         if item[1].find(".m3u8") != -1:
@@ -130,40 +119,52 @@ def main():
                 tsurls.append(m3u8_1080url.replace(
                     m3u8_1080url[start + 1: end], tmp))
 
-        cnt = 1
+        cnt = 0
         downloadsize = 0
+        poolsize = os.cpu_count()
+        start = 0
+        end = start+poolsize
         starttime = datetime.datetime.now()
-        for item in tsurls:
-            data = requests.get(item)
-            one_ts.write(data.content)
-            downloadsize += data.content.__len__()
-            nowtime = datetime.datetime.now()
-            etime = nowtime - starttime
-            tsurlslen = len(tsurls)
-            barsize = 25
-            print(
-                "\r"
-                #                + item[1]
-                #                + " "
-                + str(etime).split(".")[0]
-                + " / "
-                + str(etime / cnt * tsurlslen).split(".")[0]
-                + " {:4d}".format(cnt)
-                + " / "
-                + "{:4d}".format(tsurlslen)
-                + "  {:5}".format(
-                    int(downloadsize / (nowtime.timestamp() - starttime.timestamp()))
-                    >> 10
+
+        with Pool(poolsize) as p:
+            while start < len(tsurls):
+                tsurls_part = tsurls[start:end]
+                data = p.map(downloadts, tsurls_part)
+                # for item in data:
+                for i in range(poolsize):
+                    if i < len(data):
+                        one_ts.write(data[i].content)
+                        downloadsize += data[i].content.__len__()
+                        cnt += 1
+                nowtime = datetime.datetime.now()
+                etime = nowtime - starttime
+                tsurlslen = len(tsurls)
+                barsize = 25
+                print(
+                    "\r"
+                    #                + item[1]
+                    #                + " "
+                    + str(etime).split(".")[0]
+                    + " / "
+                    + str(etime / cnt * tsurlslen).split(".")[0]
+                    + " {:4d}".format(cnt)
+                    + " / "
+                    + "{:4d}".format(tsurlslen)
+                    + "  {:5}".format(
+                        int(downloadsize / \
+                            (nowtime.timestamp() - starttime.timestamp()))
+                        >> 10
+                    )
+                    + " KB/s"
+                    + " {:6.2f}".format(cnt * 100 / tsurlslen, 3)
+                    + " %"
+                    + ("  [{:" + str(barsize) + "s}] ").format(
+                        "|" * int((cnt / tsurlslen) * barsize)
+                    ),
+                    end="",
                 )
-                + " KB/s"
-                + " {:6.2f}".format(cnt * 100 / tsurlslen, 3)
-                + " %"
-                + ("  [{:" + str(barsize) + "s}] ").format(
-                    "|" * int((cnt / tsurlslen) * barsize)
-                ),
-                end="",
-            )
-            cnt += 1
+                start = end
+                end = start+poolsize
 
         if m3u8_1080url.find("live") != -1:
             print("\n30초간 이어지는 영상이 있는지 확인 후 저장합니다. 라이브 종료까지 반복됩니다.")
@@ -174,8 +175,24 @@ def main():
         new_content = requests.get(m3u8_1080url).content
     one_ts.close()
 
-    input("\n다운로드 완료( " + os.getcwd() + "\\" + path + " )")
+    print("\n다운로드 완료( " + os.getcwd() + "\\" + path + " )")
 
 
 if __name__ == "__main__":
+    print("\n* 저작권자의 동의 없이 콘텐츠를 무단 배포 및 가공하는 행위는 저작권 침해에 해당합니다.")
+    print("* Unauthorized or illegal distribution of paid contents, which are available exclusively to those who paid for the subscription, is punishable by law.\n")
+
+    print("필수모듈 설치확인 중")
+    try:
+        from playwright.sync_api import sync_playwright
+    except:
+        print("라이브 데이터 수집모듈이 발견되지 않아 설치합니다.")
+        install("playwright")
+        from playwright.sync_api import sync_playwright
+    try:
+        import requests
+    except:
+        print("라이브 데이터 다운로더가 발견되지 않아 설치합니다.")
+        install("requests")
+        import requests
     main()
